077cfe89b82fb05e6b73bff3efb0c89dad00b817
unboxxbusiness <unboxxbusiness@gmail.com>
2026-07-10 17:27:04 +0530

    push notificationsfix

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,11 +2,12 @@ import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   try {
-    const projectId = process.env.FIREBASE_PROJECT_ID;
-    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY
-      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-      : undefined;
+    const projectId = process.env.FIREBASE_PROJECT_ID?.replace(/(^["']|["']$)/g, "");
+    const clientEmail = process.env.FIREBASE_CLIENT_EMAIL?.replace(/(^["']|["']$)/g, "");
+    let privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/(^["']|["']$)/g, "");
+    if (privateKey) {
+      privateKey = privateKey.replace(/\\n/g, "\n");
+    }
 
     if (projectId && clientEmail && privateKey) {
       admin.initializeApp({
